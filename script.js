e42d41d0461b2172da9411b8834fe5953c5af652
chrisdindustrial <chris.caicedo@correounivalle.edu.co>
2025-07-29 23:19:34 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,27 @@
+<script type="module">
+  // Import the functions you need from the SDKs you need
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/12.0.0/firebase-app.js";
+  import { getAnalytics } from "https://www.gstatic.com/firebasejs/12.0.0/firebase-analytics.js";
+  // TODO: Add SDKs for Firebase products that you want to use
+  // https://firebase.google.com/docs/web/setup#available-libraries
+
+  // Your web app's Firebase configuration
+  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+  const firebaseConfig = {
+    apiKey: "AIzaSyDnGsR3zwxDS22OFBoyR0FPntSRnDTXkno",
+    authDomain: "planificadoruv.firebaseapp.com",
+    projectId: "planificadoruv",
+    storageBucket: "planificadoruv.firebasestorage.app",
+    messagingSenderId: "289578190596",
+    appId: "1:289578190596:web:d45140a8bd7aff44b13251",
+    measurementId: "G-WQVMB3XP2W"
+  };
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+  const analytics = getAnalytics(app);
+</script>
+
 document.addEventListener('DOMContentLoaded', () => {
 
     const initialSubjects = [
