21836b8c72a8cc12f5536adcfbaf659f1f95b8a7
chrisdindustrial <chris.caicedo@correounivalle.edu.co>
2025-08-13 22:21:08 -0500

    Delete js directory

--- js/main.js
+++
@@ -1,6 +0,0 @@
-// js/main.js
-// Este archivo DEBE cargarse primero.
-// Su única responsabilidad es crear el espacio de nombres (namespace) global
-// para evitar conflictos y errores de carga.
-
-window.App = {};
