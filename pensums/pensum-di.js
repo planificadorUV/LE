d9d8010e615573e8ec4d774ce930a0fd173022d2
chrisdindustrial <chris.caicedo@correounivalle.edu.co>
2025-10-05 18:14:04 -0500

    Update pensum-di.js

--- pensums/pensum-di.js
+++ pensums/pensum-di.js
@@ -25,11 +25,11 @@ const PENSUM_DI = [
     { id: '507111C', name: 'DISEÑO COLONIAL Y MODERNIDAD D.I.', credits: 2, type: 'AB', prerequisites: [] },
     { id: '507035C', name: 'DISIDENCIAS Y RESISTENCIAS', credits: 3, type: 'AB', prerequisites: [] },
     { id: '507036C', name: 'ESTUDIOS CRÍTICOS DEL DISEÑO', credits: 3, type: 'AB', prerequisites: [] },
-
+    { id: '507025C', name: 'PROYECTO - SER HUMANO', credits: 7, type: 'AB', prerequisites: [] },
+    { id: '507024C', name: 'PROYECTOS - RELACIONES Y VÍNCULOS', credits: 4, type: 'AB', prerequisites: [] },
+    { id: '507021C', name: 'PROYECTOS - ESTRUCTURAS Y AUTONOMÍAS', credits: 4, type: 'AB', prerequisites: [] },
     // Ciclo Profesional (Componente Profesional)
-    { id: '507025C', name: 'PROYECTO - SER HUMANO', credits: 7, type: 'AP', prerequisites: [] },
-    { id: '507024C', name: 'PROYECTOS - RELACIONES Y VÍNCULOS', credits: 4, type: 'AP', prerequisites: [] },
-    { id: '507021C', name: 'PROYECTOS - ESTRUCTURAS Y AUTONOMÍAS', credits: 4, type: 'AP', prerequisites: [] },
+
     { id: '507047C', name: 'HERRAMIENTAS DIGITALES - REPRESENTACIÓN', credits: 2, type: 'AP', prerequisites: [] },
     { id: '507014C', name: 'MATERIALES Y PROCESOS - METALES', credits: 3, type: 'AP', prerequisites: ['507012C'] },
     { id: '507011C', name: 'HERRAMIENTAS DIGITALES - PROGRAMACIÓN', credits: 2, type: 'AP', prerequisites: [] },
